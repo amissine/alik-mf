d3a3dae684536efd5cbde6ef3262622aa845b09d
Alec Missine <alec_missine@yahoo.com>
2018-09-02 18:10:12 -0400

    Run d1

--- sandbox/demo/d1-udp.c
+++ sandbox/demo/d1-udp.c
@@ -1,10 +1,3 @@
-/*
- * Acknowledgements:
- * - https://github.com/uNetworking/uSockets
- * - https://cs.nyu.edu/~mwalfish/classes/16sp/classnotes/handout01.pdf
- * - https://www.cs.rutgers.edu/~pxk/417/notes/sockets/udp.html
- */
-
 #include <udp.h>
 
 static inline void checkArgs (int argc, char** argv) {
@@ -14,14 +7,30 @@ static inline void checkArgs (int argc, char** argv) {
   }
 }
 
+#define BUFSIZE 512
+char buf[BUFSIZE];
+
 int main (int argc, char** argv) {
 
   checkArgs(argc, argv);
 
   int socketFd;
 #ifdef SERVER
-  socketFd = udpFdBnd(argv[1], argv[2]); // socket to listen on
+  socketFd = udpFdBnd(argv[2], 0);       // socket to listen on
+
+  struct sockaddr_storage peer_addr;
+  socklen_t peer_addr_len;
+  ssize_t nread;
+  for (;;) {
+    peer_addr_len = sizeof(struct sockaddr_storage);
+    nread = recvfrom(socketFd, buf, BUFSIZE, 0,
+        (struct sockaddr *) &peer_addr, &peer_addr_len);
+    printf("%s\n", buf);
+  }
 #else  // CLIENT
-  socketFd = udpFd(argv[1], argv[2]);    // socket to write to
+  socketFd = udpFd(argv[1], argv[2], 0); // socket to connect to
+
+  char *msg = "Hello UDP peer";
+  write(socketFd, msg, strlen(msg)+1);
 #endif // SERVER  
 }
