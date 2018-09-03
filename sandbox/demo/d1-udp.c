611001f10a0ca91d453a82fed6c75a0925b3797d
Alec Missine <alec_missine@yahoo.com>
2018-09-03 13:21:30 -0400

    Bug fix

--- sandbox/demo/d1-udp.c
+++ sandbox/demo/d1-udp.c
@@ -30,8 +30,8 @@ int main (int argc, char** argv) {
     char host[NI_MAXHOST], service[NI_MAXSERV];
     int s = getnameinfo((struct sockaddr *) &peer_addr, peer_addr_len,
         host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICSERV);
-    if (s == 0) printf("Received %zn bytes from %s:%s\n", nread, host, service);
-    else fprintf(stderr, "getnameinfo: %s\n", gai_stderror(s));
+    if (s == 0) printf("Received %d bytes from %s:%s\n", nread, host, service);
+    else fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
 
     if (sendto(socketFd, buf, nread, 0,
           (struct sockaddr *) &peer_addr, peer_addr_len) != nread)
